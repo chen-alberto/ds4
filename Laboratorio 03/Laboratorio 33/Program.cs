f35b1625b8471997d936b8ee75bccaf07e793011
chen-alberto <alberto14chen@gmail.com>
2025-09-16 21:22:43 -0500

    Laboratorio 3 completo

---
+++ Laboratorio 03/Laboratorio 33/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Laboratorio_33
+{
+    public class CalculosMatematicos
+    {
+        public static double PerimetroRectangulo(double lado1, double lado2)
+        {
+            return 2 * (lado1 + lado2);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el lado 1 del rectángulo: ");
+            double lado1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el lado 2 del rectángulo: ");
+            double lado2 = Convert.ToDouble(Console.ReadLine());
+
+            double perimetro = CalculosMatematicos.PerimetroRectangulo(lado1, lado2);
+            Console.WriteLine("Perímetro del rectángulo = {0}",perimetro);
+        }
+    }
+}
