bbb5d23e9745f1b5ec25bb47ca7db8b4f944943d
chen-alberto <alberto14chen@gmail.com>
2025-09-17 17:40:56 -0500

    Laboratorio 4

---
+++ Laboratorio 4/Laboratorio 46/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite el readio del circulo");
+        double radio = double.Parse(Console.ReadLine());
+
+        double area = Math.Pow(radio, 2) * Math.PI;
+
+        Console.WriteLine($"El area del circulo es: {area}");
+    }
+}
