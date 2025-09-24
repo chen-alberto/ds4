1c5ce2a4b0de23e6de935196c66301922de17859
chen-alberto <alberto14chen@gmail.com>
2025-09-23 20:13:23 -0500

    Laboratorio 6 completo

---
+++ Laboratorio 6/Laboratorio6/Laboratorio64/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+internal class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Acceso negado - no cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("acceso concedido");
+        }
+    }
+    static void Main(string[] args)
+    {
+        checkAge(15);
+    }
+}
