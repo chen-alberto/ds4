f68311ed8f0b3f4f12151f5f0e25c1b45a14b5c3
chen-alberto <alberto14chen@gmail.com>
2025-10-14 17:35:57 -0500

    Laboratorio 11 completo

---
+++ Parcial 1/Parcial 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el tamaño n de la matriz: ");
+        int n = int.Parse(Console.ReadLine());
+        int[,] matriz = new int[n, n];
+        Random random = new Random();
+        int suma = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (n % 2 == 0 && 
+                    (i == n / 2 - 1 || i == n / 2) &&
+                    (j != 0 && j != n - 1)) 
+                {
+                    matriz[i, j] = random.Next(101, 200);
+                }
+                else
+                {
+                    matriz[i, j] = 0;
+                }
+                suma += matriz[i, j];
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
