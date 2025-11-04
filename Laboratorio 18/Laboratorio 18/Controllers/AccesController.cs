9f7f86c1f4bc8df9fd22d4ff8e0b3f82147b0ccc
chen-alberto <alberto14chen@gmail.com>
2025-11-03 20:45:47 -0500

    Laboratorio 18

---
+++ Laboratorio 18/Laboratorio 18/Controllers/AccesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Laboratorio_18.Controllers
+{
+    public class AccesController : Controller
+    {
+        // GET: Acces
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Enter(String user, String password)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch(Exception ex)
+            {                 
+                return Content("Ocurrio un error:(" + ex.Message);
+            }
+        }
+    }
+}
